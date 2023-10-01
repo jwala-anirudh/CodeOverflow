3e67e7c930159625ecfb90df2ed26528ec1368f7
Anirudh Jwala <anirudhsai2@gmail.com>
2023-10-02 00:07:42 +0530

    Application authentication setup

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
 const Home = () => (
-  <>
+  <div>
+    <UserButton afterSignOutUrl='/' />
     <h1 className='h1-bold'>Next.js stackoverflow application</h1>
-  </>
+  </div>
 );
 
 export default Home;
