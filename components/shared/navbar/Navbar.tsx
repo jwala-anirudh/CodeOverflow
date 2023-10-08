99540e71030545abafef7a8af46c490db062d9a6
Anirudh Jwala <anirudhsai2@gmail.com>
2023-10-08 19:18:35 +0530

    Theme toggler

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 
 import { SignedIn, UserButton } from '@clerk/nextjs';
 
+import Theme from './Theme';
+
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -22,7 +24,8 @@ const Navbar = () => {
       </Link>
       GlobalSearch
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
+
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
