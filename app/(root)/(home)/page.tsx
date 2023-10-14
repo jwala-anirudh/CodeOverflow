032432ec4c672e40b759c7b2566ae94ba5711b5b
Anirudh Jwala <anirudhsai2@gmail.com>
2023-10-14 22:26:53 +0530

    Local search bar component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+
 const Home = () => (
-  <div>
-    <h1 className="h1-bold">Next.js stackoverflow application</h1>
-  </div>
+  <>
+    <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+      <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+      <Link href={`/ask-question`} className="flex justify-end max-sm:w-full">
+        <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+          Ask a Question
+        </Button>
+      </Link>
+    </div>
+
+    <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <LocalSearchbar
+        route="/"
+        iconPosition="left"
+        imgSrc="/assets/icons/search.svg"
+        placeholder="Search for questions"
+        otherClasses="flex-1"
+      />
+      Filters
+    </div>
+  </>
 );
 
 export default Home;
